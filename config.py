01d669949c2f7c5183194c560f842e77c1c007af
Blackmighty <gjkiioeeowo@gmail.com>
2025-04-29 18:49:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "24180264"))
 API_HASH = environ.get("API_HASH", "cedea589a0ddbf382b755852409e7bc4")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8159340701:AAHwpXfsSupjq5cJB_nQi-3AExPlsJ5CqgA")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8022307089:AAEOAH6eWmjNP4UjEyHz0DW5JrQ6fhKM1Xk")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
@@ -64,10 +64,10 @@ SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
 # Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://blackmighty.blogspot.com/2024/11/family-by-choice-2024-english-subtitles.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
 STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
